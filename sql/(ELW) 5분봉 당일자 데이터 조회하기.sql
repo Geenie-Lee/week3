5d901fd965b4bdc58cb4296dce645065611a6290
Geenie <iamyourside@gmail.com>
2021-03-22 18:32:08 +0900

    ELW 5분봉 데이터 조회하기(시분조회 추가)

--- sql/(ELW) 5분봉 당일자 데이터 조회하기.sql
+++ sql/(ELW) 5분봉 당일자 데이터 조회하기.sql
@@ -18,7 +18,7 @@ select stock_code
      , max(close_price) as max_close_price
      , min(close_price) as min_close_price
   from stock_5min_elw
- where stock_code = '57G166'
+ where stock_code in ('57G166','57G188')
  group by stock_code,stock_name;
  
 
@@ -34,7 +34,17 @@ select *
 -- (57G188) PUT
 -------------------------------------------------------------------------------
 
-select *
+select stock_code
+     , stock_name
+     , min_num
+     , substring(conclusion_time,9,2)||':'||substring(conclusion_time,11,2) as hhmm
+     , volume
+     , close_price
+     , open_price
+     , high_price
+     , low_price
+     , high_price-low_price as hl_gap_price
   from stock_5min_elw
  where stock_code = '57G188'
-   and substring(conclusion_time,1,8) = to_char(current_date,'yyyymmdd');
+   and substring(conclusion_time,1,8) = to_char(current_date,'yyyymmdd')
+ order by min_num desc;
